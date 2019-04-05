f4e5ac8afc544d5409fc8a7e921665ee1f45676d
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-04-05 12:54:02 +0300

    Add nodemon for autorestarting dev server

--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,7 @@ import { getRandomNumber, getSecureRandomNumber } from './service/random';
 export const api = express();
 
 api.get('/status', (_, res) => res.json({ status: 'OK' }));
+
 api.get('/random', (_, res) => res.json({ randomNumber: getRandomNumber() }));
 api.get('/random/secure', (_, res, next) =>
   getSecureRandomNumber()
