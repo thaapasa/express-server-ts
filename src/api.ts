42ec93f9903532cef216bc698300e78cd76d515a
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-04-05 08:06:01 +0300

    Make server listen and serve status response

---
+++ src/api.ts
@@ -0,0 +1,5 @@
+import * as express from "express"
+
+export const api = express()
+
+api.get("/status", (_, res) => res.json({ status: "OK" }))
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
 console.log("Starting server")
+
+import * as express from "express"
+import { api } from "./api";
+import * as bodyParser from "body-parser"
+import * as compression from "compression"
+
+const serverPort = 3500
+
+const server = express()
+server.use(compression())
+server.use(bodyParser.json())
+
+server.use("/api", api)
+
+server.listen(serverPort, () => {
+  console.log(`Server started at port ${serverPort}`)
+})
